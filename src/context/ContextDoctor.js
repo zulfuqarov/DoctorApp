d5b13fc1b57091343b42140b8fab06a1fa5f7905
nebi zulfuqarov <nebizulfuqarov@nebis-MacBook-Air.local>
2025-05-02 01:41:37 +0400

    fire base auth error

--- src/context/ContextDoctor.js
+++ src/context/ContextDoctor.js
@@ -1,9 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { createContext } from 'react'
+import React, { createContext, useEffect } from 'react'
+import { authDb } from '../connections/firebaseConfig'
+
+// auth import
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
 
 export const DoctorContext = createContext()
 
 const ContextDoctor = ({ children }) => {
+
+    // useEffect(() => {
+    //     const unsubscribe = onAuthStateChanged(authDb, (user) => {
+    //         if (user) {
+    //             console.log("Giriş yapmış kullanıcı:", user.email);
+    //         } else {
+    //             console.log("Kullanıcı çıkış yapmış.");
+    //         }
+    //     });
+    //     return unsubscribe;
+    // }, []);
+
+
 
     return (
         <DoctorContext.Provider value={{}}>
